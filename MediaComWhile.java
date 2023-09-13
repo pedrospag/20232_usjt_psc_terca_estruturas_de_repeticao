2d41d2850b5c79664d17b43321f48a6e9f5d66aa
Pedro Henrique <pedrospagnuolo05@gmail.com>
2023-09-12 21:04:49 -0300

    ap05 (repeticao): declaracao de variaveis

---
+++ MediaComWhile.java
@@ -0,0 +1,14 @@
+public class MediaComWhile{
+    public static void main(String... args){
+        //declara;ão de variáveis
+        //inteiros: byte (1 byte), short (2 bytes), int (4 bytes), long (8 bytes)
+        short numeroAlunos, contador;
+        //float(4 bytes) e double(8 bytes)
+        float nota1, nota2;
+        //entrada
+
+        //processamento
+
+        //saída
+    }
+}
